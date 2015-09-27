2304aa053cff7561f09a62036c2cdfe943be6db9
Your Name <you@example.com>
2015-09-27 18:40:22 -0400

    2

---
+++ num2/ConsoleApplication2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Birth Year");
+            Console.ReadLine();
+
+
+           TimeSpan BirthYear = TimeSpan.Parse(Console.ReadLine());
+
+            TimeSpan age =  TimeSpan.Parse("2015") - BirthYear;
+           TimeSpan y = TimeSpan.Parse("1994");
+            
+            if 
+            (y >= age)
+
+            { Console.WriteLine("OK!"); }
+
+            else
+            { Console.WriteLine("NO!!"); }
+
+
+
+                
+            
+        }
+    }
+}
