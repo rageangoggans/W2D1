435e9abd8f777dfa835cbc69f029af8bdb9ee1dc
Your Name <you@example.com>
2015-09-27 18:23:01 -0400

    1

---
+++ num1/Week2Day1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool lightSwitch = true;
+            bool lamp = true;
+            Console.WriteLine(lightSwitch && lamp);
+            Console.WriteLine(lightSwitch || lamp);
+            Console.WriteLine(!lightSwitch);
+            Console.WriteLine(lightSwitch ^ lamp);
+            Console.ReadLine();
+
+
+        }
+    }
+}
